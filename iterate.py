3361ce3d24456f1f0046faf8d0c4c6aa53aec6e6
Charithareddy22 <gottimukkulacharitha@gmail.com>
2025-01-09 13:39:06 +0000

    iterateven

--- iterate.py
+++ iterate.py
@@ -5,3 +5,18 @@ type(iterate)
 for i in iterate:
     if (i%20 == 0):
         print(i)
+#without generator
+def square(i):
+    for i in range(i):
+        i = i+2
+    return i
+res = square(20)
+print(res)
+#generator
+def square(i):
+    for i in range(i):
+        i = i+2
+        yield i
+square(10)
+for i in square(10):
+    print(i)
