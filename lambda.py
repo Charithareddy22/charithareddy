6b9bfd44fbc218776e1da462e16bc59db675ebd8
Charithareddy22 <gottimukkulacharitha@gmail.com>
2025-01-09 04:05:30 +0000

    Lambda

---
+++ lambda.py
@@ -0,0 +1,11 @@
+res = lambda x,y:x+y
+print(res(3,5))
+#Subtraction
+res = lambda x,y:x-y
+print(res(6,7))
+#Multiplication
+res = lambda x,y:x*y
+print(res(25,5))
+#Division
+res = lambda x,y:x/y
+print(res(45,9))
